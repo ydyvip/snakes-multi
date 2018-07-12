4763faf19d02a6e17c2ca09f9864fcffac4ecb49
Jakub Kutrzeba <jkutrzeba1@gmail.com>
2018-07-12 19:51:08 +0200

    integration of store for session

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var bodyparser = require("body-parser");
 var session = require("express-session");
+const MongoStore = require('connect-mongo')(session);
 var passport = require("passport");
 require("./API/passport_conf.js");
 
@@ -18,8 +19,20 @@ io.on("connection", function(socket){
 var login = require("./API/login.api.js");
 var register = require("./API/register.api.js");
 
+var db = require("./db.js");
+db.promise.then( function(){
+	http.listen(3004, function(){
+		console.log("App listen on 3004 port");
+	});
+})
+
 app.use(express.static("dist"));
-app.use(session({ secret: "TeraXaxzz" }));
+app.use(session({
+   secret: "TeraXaxzz",
+   store: new MongoStore({
+     dbPromise: db.promise
+   })
+}));
 app.use(bodyparser.json());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -27,13 +40,3 @@ app.use(passport.session());
 
 app.use("/login", login);
 app.use("/register", register);
-
-var db = require("./db.js");
-
-db.promise.then( function(){
-
-	http.listen(3004, function(){
-		console.log("App listen on 3004 port");
-	});
-
-})
