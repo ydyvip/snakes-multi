c04999ba07e6d15524ef3aecc441d9b693a6c494
Jakub Kutrzeba <jkutrzeba1@gmail.com>
2019-08-17 16:49:03 +0200

    Read mongo URI from json file (excluded from sources due to credentials)

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 
 var MongoClient = require('mongodb').MongoClient;
 
-var url = 'mongodb://localhost:27017/myproject';
+var url = require("./mongouri.json").mongouri;
 
 const dbName = 'myproject';
 
